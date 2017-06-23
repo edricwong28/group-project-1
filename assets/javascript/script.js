3b6d19115b9940a04768b0217fb84823e4be4c24
edricwong28 <edricwong28@gmail.com>
2017-06-22 20:41:33 -0700

    Add files via upload

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -57,12 +57,12 @@
 
     //MATERIALIZE SCRIPTS
   $(document).ready(function(){
-      $('.carousel').carousel({dist:0});
+      // $('.carousel').carousel({dist:0});
       $('.parallax').parallax();
       Materialize.updateTextFields();
       $('select').material_select();
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
-      $('.slider').slider();        
+      // $('.carousel.carousel-slider').carousel({fullWidth: true});
+      // $('.slider').slider();        
       // $('select').material_select('destroy');
   });
      
@@ -103,15 +103,15 @@
       //r2[0] is pointing to the "parent" array, which holds mulipltle objects
        r2 = r2[0];
 
-      $(".city").html(r1.name);
-      $(".temp").html(r1.main.temp);
-      $(".main").html(r1.weather[0].main);
-      $(".description").html(r1.weather[0].description);
+      $("#1").html(r1.name);
+      $("#2").html(r1.main.temp);
+      $("#3").html(r1.weather[0].main);
+      $("#4").html(r1.weather[0].description);
              //FOR WEATHER ICON IMAGE TO DISPLAY
        var iconCode = r1.weather[0].icon;
        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
        var icon = $("<img>").attr("src", iconURL);
-      $(".icon").html(icon);
+      $("#5").html(icon);
       
       // $(".show").append(r2[0].show_title);
       // $(".show").append(r2[1].show_title);
@@ -141,16 +141,17 @@
                 var newDiv = document.createElement("div")
 
                 var imgURL = r2[i].poster;
-                var imgDiv = $('<a id="imgDiv" class="carousel-item">');
+                var imgDiv = $('<a id="imgDiv" class="carousel-item carousel-item-custom">');
                 var img = $("<img>").attr("src", imgURL);
 
                 newDiv.innerHTML = r2[i].show_title;
 
-                // results.appendChild(newDiv);
+                imgDiv.append(img);
+                $('#main-carousel').append(imgDiv);
 
-                $(".imgDiv").html(img);
-                $(".carousel").prepend(imgDiv)
-                $("#results").prepend(newDiv);
+                // $(".carousel-item").prepend(img);
+                // $(".carousel").prepend(imgDiv);
+                // $("#results").prepend(newDiv);
                 
    
               }
@@ -189,10 +190,120 @@
               }
 
           }
-       
-         
+          $('#main-carousel').carousel();
+          // $('.slider').slider();
+
 
 
      });
     }
+
+
+  // // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyAvYUBg47CmdjD-J_-XYOcD_Xi2ekHg7lI",
+  //   authDomain: "netflix-group-project-1.firebaseapp.com",
+  //   databaseURL: "https://netflix-group-project-1.firebaseio.com",
+  //   projectId: "netflix-group-project-1",
+  //   storageBucket: "netflix-group-project-1.appspot.com",
+  //   messagingSenderId: "179076092851"
+  // };
+  
+  // firebase.initializeApp(config);
+
+  // var database = firebase.database();
+
+  // // handles the sign in button press
+  // function signIn () {
+  //   if (firebase.auth().currentUser) {
+
+  //     firebase.auth().signOut();
+  //   } else {
+  //     var email = $("#email").val().trim();
+  //     var password = $("#password").val().trim();
+  //     if (email.length < 4) {
+  //       alert("Please enter an email address. ");
+  //       return;
+  //     }
+  //     if (password.length < 4) {
+  //       alert("Please enter a password. ");
+  //       return;
+  //     }
+
+  //     //sign in with email and password
+  //     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+
+  //       //handle errors here
+  //       var errorCode = error.code;
+  //       var errorMessage = error.message;
+
+  //       if(errorCode === "auth/wrong-password") {
+  //         alert("Wrong password.");
+  //       } else {
+  //         alert(errorMessage);
+  //       }
+  //       console.log(error);
+
+  //     });
+
+  //   }
+  // }
+
+
+  // //handles the sign up button press
+  // function signUp() {
+  //   var email = $("#email").val().trim();
+  //   var password = $("#password").val().trim();
+  //   if(email.length < 4) {
+  //     alert("Please enter an email address.");
+  //     return;
+  //   }
+  //   if (password.length < 4) {
+  //     alert("Please enter a password.");
+  //     return;
+  //   }
+
+  //   //sign in with email and password
+  //   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    
+  //   //handle errors
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   if (errorCode == "auth/weak-password") {
+  //     alert("The password is too weak. ");
+  //   } else {
+  //     alert(errorMessage);
+  //   }
+
+  //   console.log(error);
+
+  //   });
+  // }
+
+  // //Send email verification to user
+
+  // function sendEmailVerification() {
+
+  //   firebase.auth().currentUser.sendEmailVerification().then(function() {
+  //     alert("Email Verification Sent!");
+  //   });
+  // }
+
+  // //reset password
+  // function sendPasswordReset() {
+  //   var email = $("#email").val().trim();
+
+  //   firebase.auth().sendPasswordResetEmail(email).then(function() {
+  //     alert("Password Reset Email Sent!");
+  //   }).catch(function(error) {
+  //     //handle errors
+  //     var errorCode = error.code;
+  //     var errorMessage = error.message;
+
+  //     if (errorCode == "auth/invalid-email") {
+  //       alert(errorMessage);
+  //     }
+  //     console.log(error);
+  //   });
+  // }
 
